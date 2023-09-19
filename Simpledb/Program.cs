b4c3cf241ed2de28686103433cf102973832e1e7
Estevão <estevaowerneck@gmail.com>
2023-09-19 20:19:57 -0300

    Criando a estrutura inicial do projeto

---
+++ Simpledb/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+
+    //estrutura esqueleto do projeto
+    static void Main(string[] args)
+    {
+
+
+        while (true)
+        {
+            Console.Write("$ simpledb-client> ");
+            string input = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "insert":
+
+                    break;
+
+                case "update":
+
+                    break;
+
+                case "remove":
+
+                case "search":
+
+                    break;
+
+                case "quit":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid command. Try again");
+                    break;
+            }
+        }
+    }
+}
